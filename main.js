85f758bb690430a49c356593a47d69953329adc3
HiCoders <hicodersofficial@gmail.com>
2023-05-27 06:04:59 +0530

    refactor: debugging code

--- main.js
+++ main.js
@@ -4,21 +4,27 @@ const cursor = document.querySelector(".cursor");
 document.addEventListener("mousemove", handleMousemove);
 
 function handleMousemove(e) {
-  let x = e.clientX,
-    y = e.clientY;
   const widthMid = parseInt(window.innerWidth / 2);
   const heightMid = parseInt(window.innerHeight / 2);
 
-  x = x - widthMid;
-  y = heightMid - y;
+  let x = e.clientX - widthMid;
+  let y = heightMid - e.clientY;
+
   let angle = Math.atan2(y, x) * (180 / Math.PI);
-  cursor.style.transform = `translate(${x + widthMid - 64}px, ${
-    -y + heightMid - 31
-  }px) rotate(${-angle}deg)`;
+
+  cursor.style.transform = `
+    translate(
+      ${e.clientX - 64}px, 
+      ${e.clientY - 31}px
+    ) 
+    rotate(${-angle}deg)`;
+
   const factor = 0.16;
-  text.style.textShadow = `${-x * factor}px ${
-    y * factor
-  }px 5px rgba(0, 0, 0, .4)`;
+
+  text.style.textShadow = `
+    ${-x * factor}px 
+    ${y * factor}px 
+    5px rgba(0, 0, 0, .4)`;
 
   // Debugging
 
